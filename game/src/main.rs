67ac237aa814b6e6619b3ee144a58ad4d162cfcc
Omar Sumon <73730920+worstfrontendcoder@users.noreply.github.com>
2022-02-05 14:53:51 -0600

    returned to original

--- game/src/main.rs
+++ game/src/main.rs
@@ -25,7 +25,7 @@ impl event::EventHandler for MainState {
         Ok(())
     }
 
-    fn rectangle ( ctx: &mut Context,) -> GameResult {
+    fn draw(&mut self, ctx: &mut Context) -> GameResult <()> {
         // draws a rectangle to the bottom center of the screen
         let (x, y) = graphics::drawable_size(ctx);
         let rect = graphics::Rect::new(x / 2.0 - 50.0, y - 50.0, 100.0, 100.0);
@@ -35,6 +35,7 @@ impl event::EventHandler for MainState {
         
         Ok(())
     }
+    
 }
 
 
